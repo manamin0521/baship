6291cdbe634b4659002b29752d1e3055a2e6cad8
matsubara.manami <matsubara.manami@slogan.jp>
2016-07-10 10:34:34 +0900

    やっと開発環境整った

--- db/migrate/20160710004805_create_visit_logs.rb
+++ db/migrate/20160710004805_create_visit_logs.rb
@@ -1,12 +1,10 @@
 class CreateVisitLogs < ActiveRecord::Migration
   def change
     create_table :visit_logs do |t|
-      # t.integer :spot_id
-      # t.integer :weather
-      # t.string :group_kind
-      # t.string :integer
-      # t.string :season
-      # t.string :integer
+      t.integer :spot_id
+      t.integer :weather
+      t.integer :group_kind
+      t.integer :season
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,12 @@ ActiveRecord::Schema.define(version: 20160710004805) do
   end
 
   create_table "visit_logs", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "spot_id",    limit: 4
+    t.integer  "weather",    limit: 4
+    t.integer  "group_kind", limit: 4
+    t.integer  "season",     limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
 end
